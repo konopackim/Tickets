8678c1d372c31b74c6e217ab494b0d3aad0326f7
mkonopacki <konopacki.m@gmail.com>
2015-04-08 01:48:17 +0200

    Using Sqlite database

--- scrap_artist_page.py
+++ scrap_artist_page.py
@@ -6,28 +6,7 @@ from selenium.common.exceptions import WebDriverException
 from lxml import etree
 from datetime import datetime
 import pandas as pd
-
-from scrap_ticket_page import *
-
-
-def main():
-    d_artists = {'One Direction': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/One-Direction-Tickets',
-                 'Katy Perry': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Katy-Perry-Tickets',
-                 'U2': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/U2-Tickets',
-                 'Michael Buble': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Michael-Buble-Tickets',
-                 'Madonna': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Madonna-Tickets',
-                 'Eric Clapton': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Eric-Clapton-Tickets',
-                 'Paul McCartney': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Paul-McCartney-Tickets',
-                 'Kana Nishino': 'http://www.viagogo.com/ww/Concert-Tickets/J-Pop/Kana-Nishino-Tickets'
-                 }
-
-    for s_name, url in d_artists.iteritems():
-        print s_name, url
-        df_events = scrap_artist_page(s_name, url)
-        for index, event in df_events.iterrows():
-            print event['event_name'], event['datetime']
-            e_url = event['data_navigate']
-            scrap_ticket_page('http://' + e_url)
+import logging
 
 def ajax_complete(driver):
     try:
@@ -46,7 +25,12 @@ def scrap_artist_page(s_name, url):
 
     do_click = True
     while do_click:
-        elem = browser.find_element_by_xpath('//*[@id="grid"]/div[2]')
+        try:
+            elem = browser.find_element_by_xpath('//*[@id="grid"]/div[2]')
+        except :
+            logging.warning('Button Next missing')
+            break
+
         if elem.text:
             elem.click()
             WebDriverWait(browser, 10).until(
@@ -59,6 +43,8 @@ def scrap_artist_page(s_name, url):
     browser.quit()
     # Create list of all entries with unparsed concert data
     elem_list = my_tree.xpath('//*[@id="grid"]/table/tbody/tr')
+    if len(elem_list) == 0:
+        return None
     event_list = []
     # print len(elem_list)
     for element in elem_list:
@@ -87,20 +73,5 @@ def scrap_artist_page(s_name, url):
                                                'data_navigate'])
     dFrame.set_index('event_id', inplace=True)
 
-    # Open previous dataframe
-    fName = 'events_details.csv'
-    if os.path.isfile(fName):
-        dFrame_old = pd.read_csv(fName, parse_dates=['datetime'], index_col=['event_id'], encoding='utf-8')
-        dFrame_old = dFrame_old.append(dFrame)
-        dFrame_old.drop_duplicates(inplace=True)
-        # dFrame_old.groupby().first()
-    else:
-        dFrame_old = dFrame
-
-    # dFrame_old.sortlevel(inplace=True)
-    dFrame_old.sort(columns=['event_name', 'datetime'], inplace=True)
-    dFrame_old.to_csv(fName, encoding='utf-8')
     return dFrame
 
-if __name__ == "__main__":
-    main()
---
+++ tickets_parser.py
@@ -0,0 +1,62 @@
+__author__ = 'Dell'
+import sqlalchemy
+from scrap_artist_page import *
+from scrap_ticket_page import *
+import pandas as pd
+from datetime import datetime
+import sqlite3
+import logging
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    eng = sqlalchemy.create_engine('sqlite:///tickets2.db')
+    # eng = sqlite3.connect("tickets.db")
+    # print eng.table_names()
+    d_artists = {'One Direction': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/One-Direction-Tickets',
+                 'Katy Perry': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Katy-Perry-Tickets',
+                 'U2': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/U2-Tickets',
+                 'Michael Buble': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Michael-Buble-Tickets',
+                 'Madonna': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Madonna-Tickets',
+                 'Eric Clapton': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Eric-Clapton-Tickets',
+                 'Paul McCartney': 'http://www.viagogo.com/ww/Concert-Tickets/Rock-and-Pop/Paul-McCartney-Tickets',
+                 'Kana Nishino': 'http://www.viagogo.com/ww/Concert-Tickets/J-Pop/Kana-Nishino-Tickets'
+                 }
+
+    # Get all info about events
+    # and add them to the database
+    df_events = pd.read_sql('events', eng, index_col='event_id')
+    df_events_today = pd.DataFrame()
+    for s_name, url in d_artists.iteritems():
+        logging.info('%s %s' % (s_name, url))
+        df_event = scrap_artist_page(s_name, url)
+        if df_event is None:
+            continue
+        logging.info(df_event.count())
+        # if len(df_events_today) == 0:
+        #     df_events_today = df_event
+        # else:
+        df_events_today = df_events_today.append(df_event)
+    df_events.append(df_events_today)
+    df_events.drop_duplicates()
+    # df_events.reset_index().sort_index(by=['event_name', 'event_id'], ascending=[True, True]).set_index('event_id', inplace=True)
+    # logging.info(df_events)
+    # events_navigate = df_events[['event_name', 'data_navigate']].unique()
+    df_events.to_sql('events', eng, if_exists='replace', index=True)
+    df_events.to_csv('logs\events_%s.csv' % datetime.today().date(), encoding='utf-8')
+
+    tickets_today = pd.DataFrame()
+    for index, event in df_events_today.iterrows():
+        logging.info('%s %s %s' % (event['event_name'], event['datetime'], datetime.now().time()))
+        e_url = event['data_navigate']
+        tickets = scrap_ticket_page('http://' + e_url)
+        # logging.info(tickets)
+        tickets_today = tickets_today.append(tickets)
+    logging.info(tickets_today.count())
+    logging.info(tickets_today.dtypes)
+    logging.info('%s: Started: Sending tickets data into database' % datetime.now().time())
+    tickets_today.to_sql('tickets', eng, if_exists='append', index=True)
+    logging.info('%s: Finished: Sending tickets data into database' % datetime.now().time())
+
+
+if __name__ == "__main__":
+    main()
